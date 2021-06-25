f1853cc14c7cc6a1b9089308c8e5144c4edf6f25
Blagovest Todorov <72020361+Blagovest-Todorov@users.noreply.github.com>
2021-06-25 10:44:39 +0300

    Update Student.cs

--- Student.cs
+++ Student.cs
@@ -6,14 +6,12 @@ namespace ClassroomProject
 {
     public class Student
     {
-
         public Student(string firstName, string lastName, string subject)
         {
             this.FirstName = firstName;
             this.LastName = lastName;
             this.Subject = subject;
         }
-
 
         public string FirstName { get; set; }
         public string LastName { get; set; }
